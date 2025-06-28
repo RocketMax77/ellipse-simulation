2e6b2c9c4ef0eb4fb04b2d42ade4c8105181e238
max <cybermaxwell07@gmail.com>
2025-06-28 15:32:56 -0400

    Yep good enough

--- ellipseAni.py
+++ ellipseAni.py
@@ -21,39 +21,38 @@ xVals = list(np.arange(-1*majorAxis, majorAxis, 0.01))
 xVals.reverse()
 yVals = [ellipseFunc(majorAxis, minorAxis, x) for x in xVals]
 xVals2 = list(np.arange(-1*majorAxis, majorAxis, 0.01))
-yVals2 = [ellipseFunc(majorAxis, minorAxis, x, True) for x in xVals]
+yVals += [ellipseFunc(majorAxis, minorAxis, x, True) for x in xVals2]
 xVals += xVals2
-yVals += yVals2
-numFrames = len(xVals)
+numFrames = len(yVals)
 
 
 #Ploting mangos
 fig, ax = plt.subplots()
 plt.style.use('seaborn-v0_8')
 plt.grid(True)
-ellipse = ax.plot(xVals[0], yVals[0], color='black')[0]
-lineF1 = ax.plot(-1*fociDist, 0, color='red')[0]
-lineF2 = ax.plot(fociDist, 0, color='red')[0]
-lineTV1 = ax.plot(xVals[0], 0, linestyle='dashed')[0]
+ellipse = ax.plot(xVals[0], yVals[0], color='black')[0] #The actual ellipse
+lineF1 = ax.plot(-1*fociDist, 0, color='red')[0] #Foci one
+lineF2 = ax.plot(fociDist, 0, color='red')[0] #Foci two
+lineTV1 = ax.plot(xVals[0], 0, linestyle='dashed')[0] #Vertial line to show right triangles
 ax.set(xlim=[-6,6], ylim=[-6,6])
 
 #animates the plot
 def animateElipse(frame):
-    '''A helper function that animates the plot'''
+    '''A function that animates the plot'''
     
     x = xVals[:frame]
     y = yVals[:frame]
     ellipse.set_xdata(x)
     ellipse.set_ydata(y)
 
-    xline = [-1*fociDist, xVals[frame]]
-    yline = [0, yVals[frame]]
-    xline2 = [fociDist, xVals[frame]]
-    yline2 = [0, yVals[frame]]
-    lineF1.set_xdata(xline)
-    lineF1.set_ydata(yline)
-    lineF2.set_xdata(xline2)
-    lineF2.set_ydata(yline2)
+    xF1 = [-1*fociDist, xVals[frame]]
+    yF1 = [0, yVals[frame]]
+    xF2 = [fociDist, xVals[frame]]
+    yF2 = [0, yVals[frame]]
+    lineF1.set_xdata(xF1)
+    lineF1.set_ydata(yF1)
+    lineF2.set_xdata(xF2)
+    lineF2.set_ydata(yF2)
 
     xlineTV1 = [xVals[frame], xVals[frame]]
     ylineTV1 = [0, yVals[frame]]
